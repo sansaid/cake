ff0387a65d0f541b37a1227e70eb7250563d9946
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-02-14 00:40:07 +0000

    Getting commands to work

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"factories"
+	"log"
+	"os"
 
 	"github.com/mitchellh/cli"
+	"github.com/sansaid/cake/cmds"
 )
 
 // NOTE: https://pkg.go.dev/github.com/mitchellh/cli#CLI
@@ -12,10 +14,28 @@ import (
 type cliFactory map[string]cli.CommandFactory
 
 func main() {
-	cake := cli.NewCli("cake", "0.1.0")
+	cake := cli.NewCLI("cake", "0.1.0")
+
+	cake.Args = os.Args[1:]
+	config, _ := cmds.ParseArgs(cake.Args)
 
 	cake.Commands = cliFactory{
-		"start": factories.StartFactory,
+		"start": func() (cli.Command, error) {
+			return cmds.StartCommand{
+				Config: config,
+			}, nil
+		},
+		"stop": func() (cli.Command, error) {
+			return cmds.StopCommand{
+				Config: config,
+			}, nil
+		},
 	}
 
+	exitStatus, err := cake.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	os.Exit(exitStatus)
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,7 @@
 package main_test
+
+// cake init -registry=https://hub.docker.com/
+
+// cake agent sansaid/debotbot -tag=latest
+// cake noupdate sansaid/debotbot
+// cake stop sandsaid/debotbot
