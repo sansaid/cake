1910299ea479f37408abba70ebeda1955e6b0a9a
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-03-19 00:39:17 +0000

    Created a decoder function for the HTTP requests

--- main.go
+++ main.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
+
+	"github.com/sansaid/cake/cmds"
 )
 
 func main() {
 	startCmd := flag.NewFlagSet("start", flag.ExitOnError)
-	startImageFlag := startCmd.String("image", "", "[Required] The target image for the watched container, including its register. Example: sansaid/debotbot:latest")
+	startImage := startCmd.String("image", "", "[Required] The target image for the watched container, including its register. Example: sansaid/debotbot:latest")
 
 	stopCmd := flag.NewFlagSet("stop", flag.ExitOnError)
-	stopImageFlag := stopCmd.String("image", "", "[Required] The target image to stop watching")
 
 	if len(os.Args) < 2 {
-		fmt.Errorf("Expected at least one of the following subcommands: start, stop")
-		os.Exit(1)
+		cmds.ErrExit(cmds.ErrUnrecognisedSubcommands)
 	}
+
+	var cake *Cake
 
 	switch os.Args[1] {
 	case "start":
 		startCmd.Parse(os.Args[2:])
-		cake := cmds.NewConfig(*image)
+		cake = NewCake(*startImage)
 		cake.Run()
 	case "stop":
 		stopCmd.Parse(os.Args[2:])
+
+		if cake != nil {
+			cake.Stop()
+		} else {
+			cmds.ErrExit(cmds.ErrCakeNotFound)
+		}
 	default:
-		fmt.Errorf("Expected at least one of the following subcommands: start, stop")
-		os.Exit(1)
+		cmds.ErrExit(cmds.ErrUnrecognisedSubcommands)
 	}
 }
