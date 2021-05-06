b4aadf97d806acc48d3bb114362369fcbb4c9b5c
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-05-07 00:08:26 +0100

    Writing first unit test

--- lib/cake_test.go
+++ lib/cake_test.go
@@ -1,4 +1,48 @@
 package lib
 
-// - create randomizer function for []Image to test getLatestImageDigest
-// - test implementation of ByLastPushedDesc sort.Interface
+import (
+	"testing"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	dockerClient "github.com/docker/docker/client"
+)
+
+func TestIsLatestDigestPulledOK(t *testing.T) {
+	cake := &Cake{
+		Repo:              "test/repo",
+		Tag:               "sometag",
+		Registry:          "test.registry",
+		DockerClient:      &dockerClient.Client{},
+		PreviousDigest:    "",
+		LatestDigest:      "TestLatestDigest",
+		LastChecked:       time.Time{},
+		LastUpdated:       time.Time{},
+		ContainersRunning: map[string]int{},
+		StopTimeout:       time.Duration(30),
+	}
+
+	// Mocking listImages function inside Cake.IsLatestDigestPulled()
+	listImages = func(_ *dockerClient.Client) []types.ImageSummary {
+		// Some white box testing here - only care about defining the RepoDigests field
+		// since that's the only field used in the function
+		return []types.ImageSummary{
+			types.ImageSummary{
+				RepoDigests: []string{"TestLatestDigest", "ADigest"},
+			},
+			types.ImageSummary{
+				RepoDigests: []string{"BDigest", "CDigest"},
+			},
+			types.ImageSummary{
+				RepoDigests: []string{"DDigest"},
+			},
+		}
+	}
+
+	result := cake.IsLatestDigestPulled()
+	expect := true
+
+	if result != expect {
+		t.Fail()
+	}
+}
