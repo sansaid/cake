e3f24c2a101eff2414a9851138940c8c9fc65273
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-08-29 19:31:18 +0100

    Removing the rubbish and starting from scratch

--- caked/logger.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	log "github.com/sirupsen/logrus"
-)
-
-func init() {
-	log.SetReportCaller(true)
-}
--- caked/repo_list.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type RepoList struct {
-	Count    int            `json:"count"`
-	Next     string         `json:"next"`
-	Previous string         `json:"previous"`
-	Results  []ImageDetails `json:"results"`
-}
