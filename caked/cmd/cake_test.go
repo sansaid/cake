2cbc78fd5bbef256c7eb205a5d10c37042ebbd67
Sanyia Saidova <sanyia.saidova@gmail.com>
2021-09-07 23:33:10 +0100

    Adding mocks and unit tests

---
+++ caked/cmd/cake_test.go
@@ -0,0 +1,226 @@
+package cmd
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	cakeMocks "github.com/sansaid/cake/caked/mocks"
+	"github.com/sansaid/cake/caked/pb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestAddNewSlice_OK(t *testing.T) {
+	var wg *sync.WaitGroup
+	_, cancel := context.WithCancel(context.Background())
+
+	slices := []*pb.Slice{
+		{ImageName: "sansaid/testimageuno:1234"},
+		{ImageName: "sansaid/testimagedos:4567"},
+	}
+
+	cake := NewCake()
+
+	for _, slice := range slices {
+		cake.AddNewSlice(slice, wg, cancel)
+	}
+
+	assert.Len(t, cake.RunningSlices.slices, 2)
+	assert.Contains(t, cake.RunningSlices.slices, "sansaid/testimageuno:1234", "sansaid/testimagedos:4567")
+}
+
+func TestMarshallHttp_OK(t *testing.T) {
+	mockHttpClient := new(cakeMocks.CakeHTTPClient)
+
+	req, _ := http.NewRequest(http.MethodGet, "test.url", nil)
+
+	mockHttpClient.On("Do", req).Return(
+		&http.Response{
+			Body: io.NopCloser(bytes.NewBufferString("{ \"fieldA\": \"valA\", \"fieldB\": \"valB\" }")),
+		},
+		nil,
+	)
+
+	jsonStruct := &struct {
+		FieldA string `json:"fieldA"`
+		FieldB string `json:"fieldB"`
+	}{}
+
+	cake := NewCake(WithHttpClient(mockHttpClient))
+
+	cake.MarshalHttp("test.url", jsonStruct)
+
+	assert.Equal(t, "valA", jsonStruct.FieldA)
+	assert.Equal(t, "valB", jsonStruct.FieldB)
+}
+
+func TestMarshallHttp_ErrorOnResponse(t *testing.T) {
+	mockHttpClient := new(cakeMocks.CakeHTTPClient)
+
+	req, _ := http.NewRequest(http.MethodGet, "test.url", nil)
+
+	mockHttpClient.On("Do", req).Return(
+		&http.Response{
+			Body: io.NopCloser(bytes.NewBufferString("{ \"fieldA\": \"valA\", \"fieldB\": \"valB\" }")),
+		},
+		fmt.Errorf("test - error on response"),
+	)
+
+	jsonStruct := &struct {
+		FieldA string `json:"fieldA"`
+		FieldB string `json:"fieldB"`
+	}{}
+
+	cake := NewCake(WithHttpClient(mockHttpClient))
+
+	assert.Error(t, cake.MarshalHttp("test.url", jsonStruct))
+}
+
+func TestMarshallHttp_MissingJsonFields(t *testing.T) {
+	mockHttpClient := new(cakeMocks.CakeHTTPClient)
+
+	req, _ := http.NewRequest(http.MethodGet, "test.url", nil)
+
+	mockHttpClient.On("Do", req).Return(
+		&http.Response{
+			Body: io.NopCloser(bytes.NewBufferString("{ \"fieldC\": \"valA\", \"fieldD\": \"valB\" }")),
+		},
+		nil,
+	)
+
+	jsonStruct := &struct {
+		FieldA string `json:"fieldA"`
+		FieldB string `json:"fieldB"`
+	}{}
+
+	cake := NewCake(WithHttpClient(mockHttpClient))
+
+	err := cake.MarshalHttp("test.url", jsonStruct)
+
+	assert.Equal(t, "", jsonStruct.FieldA)
+	assert.Equal(t, "", jsonStruct.FieldB)
+	assert.NoError(t, err)
+}
+
+func TestMarshallHttp_InvalidJson(t *testing.T) {
+	mockHttpClient := new(cakeMocks.CakeHTTPClient)
+
+	req, _ := http.NewRequest(http.MethodGet, "test.url", nil)
+
+	mockHttpClient.On("Do", req).Return(
+		&http.Response{
+			Body: io.NopCloser(bytes.NewBufferString("bad json")),
+		},
+		nil,
+	)
+
+	jsonStruct := &struct {
+		FieldA string `json:"fieldA"`
+		FieldB string `json:"fieldB"`
+	}{}
+
+	cake := NewCake(WithHttpClient(mockHttpClient))
+
+	assert.Error(t, cake.MarshalHttp("test.url", jsonStruct))
+}
+
+func TestListRunningContainerIds_Matches(t *testing.T) {
+	mockDockerClient := new(cakeMocks.CakeContainerAPIClient)
+	mockDockerClient.On("ContainerList", mock.Anything, mock.Anything).Return(
+		[]types.Container{
+			{
+				ID:    "Match1",
+				Image: "testImage@testDigest",
+			},
+			{
+				ID:    "Match2",
+				Image: "testImage@testDigest",
+			},
+			{
+				ID:    "NotMatch",
+				Image: "testImage@notTestDigest",
+			},
+		},
+		nil,
+	)
+
+	cake := NewCake(WithDockerClient(mockDockerClient))
+
+	expRes := []string{"Match1", "Match2"}
+	res, err := cake.ListRunningContainerIds("testImage", "testDigest")
+
+	assert.Equal(t, expRes, res)
+	assert.Equal(t, nil, err)
+}
+
+func TestListRunningContainerIds_NoMatches(t *testing.T) {
+	mockDockerClient := new(cakeMocks.CakeContainerAPIClient)
+	mockDockerClient.On("ContainerList", mock.Anything, mock.Anything).Return(
+		[]types.Container{
+			{
+				ID:    "NotMatch1",
+				Image: "testImage@notTestDigest",
+			},
+			{
+				ID:    "NotMatch2",
+				Image: "testImage@notTestDigest",
+			},
+		},
+		nil,
+	)
+
+	cake := NewCake(WithDockerClient(mockDockerClient))
+
+	expRes := []string{}
+	res, err := cake.ListRunningContainerIds("testImage", "testDigest")
+
+	assert.Equal(t, expRes, res)
+	assert.Equal(t, nil, err)
+}
+
+func TestListRunningContainerIds_Errors(t *testing.T) {
+	mockDockerClient := new(cakeMocks.CakeContainerAPIClient)
+	mockDockerClient.On("ContainerList", mock.Anything, mock.Anything).Return(
+		[]types.Container{},
+		errors.New("test error"),
+	)
+
+	cake := NewCake(WithDockerClient(mockDockerClient))
+
+	expRes := []string{}
+	res, err := cake.ListRunningContainerIds("testImage", "testDigest")
+
+	assert.Equal(t, expRes, res)
+	assert.Error(t, err)
+}
+
+// TODO - NEXT: finish writing tests for GetLatestDigest ->
+// Need to read from fixtures for OK tests
+// Need to write test for when MarshallHttp returns error
+func TestGetLatestDigest_OK(t *testing.T) {
+	mockHttpClient := new(cakeMocks.CakeHTTPClient)
+
+	req, _ := http.NewRequest(http.MethodGet, "test.url", nil)
+
+	mockHttpClient.On("Do", req).Return(
+		&http.Response{
+			Body: io.NopCloser(bytes.NewBufferString("{ \"fieldA\": \"valA\", \"fieldB\": \"valB\" }")),
+		},
+		nil,
+	)
+
+	cake := NewCake(WithHttpClient(mockHttpClient))
+
+	expRes := []string{}
+	res, err := cake.ListRunningContainerIds("testImage", "testDigest")
+
+	assert.Equal(t, expRes, res)
+	assert.Error(t, err)
+}
--- caked/go.mod
+++ caked/go.mod
@@ -20,6 +20,7 @@ require (
 require (
 	github.com/Microsoft/go-winio v0.5.0 // indirect
 	github.com/containerd/containerd v1.5.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
@@ -32,15 +33,26 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/zerolog v1.18.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/vektra/mockery/v2 v2.9.0 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/tools v0.1.2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
